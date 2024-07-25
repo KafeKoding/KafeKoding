21391c1edb60e7683a7c2b3a84a71dd8ef40cf18
karim.2501 <karimmitra0@gmail.com>
2024-07-25 15:59:48 +0700

    update: Mengubah framework dari react ke next js serta mencoba deploy ke firebase

---
+++ src/app/(landing)/_sections/index.ts
@@ -0,0 +1,7 @@
+import Hero from './hero/hero'
+import About_us from './about-us/about-us'
+import Platform from './platform/platform'
+import ShowCase from './show-case/show-case'
+import RowClass from './row-class/row-class'
+
+export{Hero,About_us,Platform,ShowCase,RowClass}
